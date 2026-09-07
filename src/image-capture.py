de8c25bbcc533a938ae6d2e4b22e5e5aaffea5f0
Parker Lee <parker.a.lee889@gmail.com>
2026-09-07 12:15:22 -0500

    WIP, working on a window to display the webcam

--- src/image-capture.py
+++ src/image-capture.py
@@ -1,7 +1,16 @@
 import cv2 as cv
 
+# create the webcam object
+# the variable passed in tells where to look for input
 webcam = cv.VideoCapture(0)
 if (webcam.isOpened()):
     print("webcam is open")
 else:
     print("couldn't get an input")
+
+
+
+webcam_read = webcam.read()
+while(webcam_read[0]):
+    # displays a window with the webcam output
+    display = cv.imshow("webcam feed", webcam_read[1])
